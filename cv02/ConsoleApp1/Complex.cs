c1f1fdf0403a674ce0b622e912b4ab42cfa4a38a
lachtanecicek <256807@vutbr.cz>
2025-02-19 17:52:08 +0100

    cv02

---
+++ cv02/ConsoleApp1/Complex.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Complex
+{
+    public double Realna;
+    public double Imaginarni;
+
+    public Complex(double realna = 0.0, double imaginarni = 0.0)
+    {
+        Realna = realna;
+        Imaginarni = imaginarni;
+    }
+
+    public static Complex operator +(Complex a, Complex b)
+    {
+        return new Complex(a.Realna + b.Realna, a.Imaginarni + b.Imaginarni);
+    }
+
+    public static Complex operator -(Complex a, Complex b)
+    {
+        return new Complex(a.Realna - b.Realna, a.Imaginarni - b.Imaginarni);
+    }
+
+    public static Complex operator -(Complex a)
+    {
+        return new Complex(-a.Realna, -a.Imaginarni);
+    }
+
+    public static Complex operator *(Complex a, Complex b)
+    {
+        return new Complex(a.Realna * b.Realna - a.Imaginarni * b.Imaginarni, a.Realna * b.Imaginarni + a.Imaginarni * b.Realna);
+    }
+
+    public static Complex operator /(Complex a, Complex b)
+    {
+        double jmenovatel = b.Realna * b.Realna + b.Imaginarni * b.Imaginarni;
+        double vyslRealna = (a.Realna * b.Realna + a.Imaginarni * b.Imaginarni) / jmenovatel;
+        double vyslImaginarni = (a.Imaginarni * b.Realna - a.Realna * b.Imaginarni) / jmenovatel;
+        return new Complex(vyslRealna, vyslImaginarni);
+    }
+
+    public static bool operator ==(Complex a, Complex b)
+    { 
+        return a.Realna == b.Realna && a.Imaginarni == b.Imaginarni;
+    }
+
+
+    public static bool operator !=(Complex a, Complex b)
+    {
+        return !(a == b);
+    }
+
+    public override string ToString()
+    {
+        if (Imaginarni < 0)
+        {
+            return string.Format("{0} - {1}j", Realna, -Imaginarni);
+        }
+        else
+        {
+            return string.Format("{0} + {1}j", Realna, Imaginarni);
+        }
+    }
+
+    public Complex Sdruzene()
+    {
+        return new Complex(Realna, -Imaginarni);
+    }
+
+    public double Modul()
+    { 
+        return Math.Sqrt(Realna * Realna + Imaginarni * Imaginarni);
+    }
+
+    public double Argument()
+    { 
+        return Math.Atan2(Imaginarni, Realna);
+    }
+
+}
---
+++ cv02/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿TestComplex.Test(new Complex(3.0, 5.0), new Complex(2.0, 1.0) + new Complex(1.0, 4.0), "operator +");
+TestComplex.Test(new Complex(1.0, -3.0), new Complex(2.0, 1.0) - new Complex(1.0, 4.0), "operator -");
+
+TestComplex.Test(new Complex(-2.0, -1.0), - new Complex(2.0, 1.0), "operator unarni -");
+
+Console.WriteLine("Operator ==: {0}", new Complex(2.0, 1.0) == new Complex(2.0, 1.0));
+
+Console.WriteLine("Modul: {0}", new Complex(2.0, 1.0).Modul());
+Console.WriteLine("Modul: {0}", new Complex(2.0, 1.0).Argument());
