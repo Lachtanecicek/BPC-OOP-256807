609d728a18308140e23eaf77897299b081c2381e
Filip Švehla <256807@vutbr.cz>
2025-02-26 20:34:45 +0100

    cv03 hotovo

---
+++ cv03/ConsoleApp1/Matrix.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Matrix
+    {
+        private double[,] data;
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public Matrix(double[,] values)
+        {
+            Rows = values.GetLength(0);
+            Cols = values.GetLength(1);
+            data = (double[,])values.Clone();
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+            {
+                Console.WriteLine("Chyba: Matice musí mít stejné rozměry pro sčítání.");
+                return null;
+            }
+
+            double[,] result = new double[a.Rows, a.Cols];
+            for (int i = 0; i < a.Rows; i++)
+                for (int j = 0; j < a.Cols; j++)
+                    result[i, j] = a.data[i, j] + b.data[i, j];
+
+            return new Matrix(result);
+        }
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+            {
+                Console.WriteLine("Chyba: Matice musí mít stejné rozměry pro odčítání.");
+                return null;
+            }
+            double[,] result = new double[a.Rows, a.Cols];
+            for (int i = 0; i < a.Rows; i++)
+                for (int j = 0; j < a.Cols; j++)
+                    result[i, j] = a.data[i, j] - b.data[i, j];
+
+            return new Matrix(result);
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.Cols != b.Rows)
+            {
+                Console.WriteLine("Chyba: Nevhodné rozměry matic pro násobení");
+                return null;
+            }
+               
+
+            double[,] result = new double[a.Rows, b.Cols];
+            for (int i = 0; i < a.Rows; i++)
+                for (int j = 0; j < b.Cols; j++)
+                    for (int k = 0; k < a.Cols; k++)
+                        result[i, j] += a.data[i, k] * b.data[k, j];
+
+            return new Matrix(result);
+        }
+
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Cols != b.Cols)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < a.Rows; i++)
+                for (int j = 0; j < a.Cols; j++)
+                    if (a.data[i, j] != b.data[i, j])
+                        return false;
+
+            return true;
+        }
+
+        public static bool operator !=(Matrix a, Matrix b) => !(a == b);
+
+        public static Matrix operator -(Matrix a)
+        {
+            double[,] result = new double[a.Rows, a.Cols];
+            for (int i = 0; i < a.Rows; i++)
+                for (int j = 0; j < a.Cols; j++)
+                    result[i, j] = -a.data[i, j];
+
+            return new Matrix(result);
+        }
+
+        public double Determinant()
+        {
+            if (Rows != Cols)
+            {
+                Console.WriteLine("Determinant lze vypočítat pouze ze čtvercové matice.");
+                return 0;
+            }
+                
+            if (Rows > 3)
+            {
+                Console.WriteLine("Výpočet determinantu je podporován pouze do velikosti matice 3x3.");
+                return 0;
+            }
+             
+
+            if (Rows == 1) return data[0, 0];
+            if (Rows == 2) return data[0, 0] * data[1, 1] - data[0, 1] * data[1, 0];
+
+            return data[0, 0] * (data[1, 1] * data[2, 2] - data[1, 2] * data[2, 1])
+                 - data[0, 1] * (data[1, 0] * data[2, 2] - data[1, 2] * data[2, 0])
+                 + data[0, 2] * (data[1, 0] * data[2, 1] - data[1, 1] * data[2, 0]);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                    result += data[i, j] + " ";
+                result += "\n";
+            }
+            return result;
+        }
+    }
+
+}
---
+++ cv03/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        double[,] values1 = { { 1, 1, 1 }, { 3, 4, 0 }, { 7, 6, 7 } };
+        double[,] values2 = { { 5, 6, 4 }, { 7, 2, 5 }, { 9, 5, 11 } };
+
+        Matrix m1 = new Matrix(values1);
+        Matrix m2 = new Matrix(values2);
+
+        Console.WriteLine("Součet matic A a B:");
+        Console.WriteLine(m1 + m2);
+        
+       
+        Console.WriteLine("Rozdíl matic A a B:");
+        Console.WriteLine(m1 - m2);
+        
+
+        Console.WriteLine("Násobení matic A a B:");
+        Console.WriteLine(m1 * m2);
+        
+
+        Console.WriteLine("Jsou matice A a B stejné?");
+        Console.WriteLine(m1 == m2);
+        Console.WriteLine();
+
+        Console.WriteLine("Jsou matice A a B rozdílné?");
+        Console.WriteLine(m1 != m2);
+        Console.WriteLine();
+
+        Console.WriteLine("Matice A po použití unárního operátoru:");
+        Console.WriteLine(-m1);
+        
+
+        Console.WriteLine("Determinant matice A:");
+        Console.WriteLine(m1.Determinant());
+    }
+}
